4f6ed730d78074239953699531d4e7597f5b31dc
ambekar92 <santhoshkcse4@gmail.com>
2020-10-04 15:41:10 +0530

    pi changes

--- src/controllers/test/test.js
+++ src/controllers/test/test.js
@@ -40,13 +40,15 @@ const getUsers = (request, response) => {
      console.log(request.query);
 
       var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-      var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+      var LED = new Gpio(17, 'out'); //use GPIO pin 4, and specify that it is output
       var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
 
       function blinkLED() { //function to start blinking
         if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+          console.log('ON');
           LED.writeSync(1); //set pin state to 1 (turn LED on)
         } else {
+          console.log('OFF');
           LED.writeSync(0); //set pin state to 0 (turn LED off)
         }
       }
