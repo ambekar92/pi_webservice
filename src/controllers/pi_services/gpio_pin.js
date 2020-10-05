1fec5f824de2fdd59e33314fad5a85c5c22f0f28
Santhosh <santhoshkcse4@gmail.com>
2020-10-05 17:31:52 +0530

    updated the GPIO status

--- src/controllers/pi_services/gpio_pin.js
+++ src/controllers/pi_services/gpio_pin.js
@@ -1,22 +1,40 @@
 const con = require('../../config/db.js')
 
   const gpio_pin = (request, response) => {
-     console.log('Running GPIO PIN');
+     
      var gpio_pin = request.query.gpio_pin;
      var value = request.query.value;
      console.log(request.query);
-
-      var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-      var LED = new Gpio(gpio_pin, 'out'); //use GPIO pin 4, and specify that it is output
+     console.log('GPIO PIN');
+     
+     var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+     var LED = new Gpio(gpio_pin, 'out'); //use GPIO pin 4, and specify that it is output
      
       if(value==1){
-        LED.writeSync(1); //set pin state to 1 (turn LED on)
+       LED.writeSync(1); //set pin state to 1 (turn LED on)
+       
+        let res={
+          "GPIO PIN":gpio_pin,
+          "Value":value,
+          "Status":'Running'
+        };
+        response.json(res);
+        
       }
       
       if(value==0){
         LED.writeSync(0); //set pin state to 0 (turn LED off)
         LED.unexport(); // Unexport GPIO to free resources
+      
+        let res={
+          "GPIO PIN":gpio_pin,
+          "Value":value,
+          "Status":'Stopped'
+        };
+        response.json(res);
       }
+
+      response.end();
      
   }
   
